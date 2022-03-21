7c4dcaea2567237209cfd221405edf067cc5bbd1
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-03-21 14:36:23 +0700

    Update hand.py

--- hand.py
+++ hand.py
@@ -7,3 +7,9 @@ import cvzone
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
+
+detector = HandDetector(detectionCon=0.8, maxHands=1)
+
+x = [300, 245, 200, 170, 145, 130, 112, 103, 93, 87, 80, 75, 70, 67, 62, 59, 57]
+y = [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
+coff = np.polyfit(x, y, 2)
