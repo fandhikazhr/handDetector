a0e43308b82a4ac3367f75ca7e0d00c00942ad63
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-04-20 00:30:32 +0700

    Update hands.py

--- handTracking/hands.py
+++ handTracking/hands.py
@@ -21,3 +21,12 @@ while True:
     if results.multi_hand_landmarks:
         for handLM in results.multi_hand_landmarks:
             for id, lm in enumerate(handLM.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id, cx, cy)
+                if id == 12 or id == 8:
+                    cv2.circle(img, (cx,cy), 10, (0, 255, 0), 5)
+            mpDraw.draw_landmarks(img, handLM, mpHands.HAND_CONNECTIONS)
+            # mpDraw.draw_landmarks(img, handLM, mpHands.HAND_CONNECTIONS, mpDrawStyles.get_default_hand_landmarks_style(), 
+                # mpDrawStyles.get_default_hand_connections_style())
+        print("\n")
