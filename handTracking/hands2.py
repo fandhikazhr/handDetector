0dcb3df0f6a7a84697d405a993f77d00718b0d11
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-04-22 05:52:25 +0700

    Update hands2.py

--- handTracking/hands2.py
+++ handTracking/hands2.py
@@ -3,3 +3,11 @@ import mediapipe as mp
 import time
 
 cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDrawStyles = mp.solutions.drawing_styles
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
