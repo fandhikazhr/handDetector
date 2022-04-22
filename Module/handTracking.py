857a2e5da3052942f98e6305567a90a6c7a3da41
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-04-23 04:10:25 +0700

    Update handTracking.py

--- Module/handTracking.py
+++ Module/handTracking.py
@@ -1,3 +1,15 @@
 import cv2
 import mediapipe as mp
 import time
+
+class handTrack():
+    def __init__(self, mode=False, maxHands=2, minDetectConfidence=0.5, minTrackConfidence=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.minDetectConfidence = minDetectConfidence
+        self.minTrackConfidence = minTrackConfidence
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands()
+        self.mpDrawStyles = mp.solutions.drawing_styles
+        self.mpDraw = mp.solutions.drawing_utils
